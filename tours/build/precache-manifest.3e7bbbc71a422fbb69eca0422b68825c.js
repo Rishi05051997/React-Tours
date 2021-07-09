23e70fa76aa35526b7221a9bef6cd1138f69bef5
rishi05051997 <vrushabhdhatral10@gmail.com>
2021-07-09 13:07:01 +0530

    build

---
+++ tours/build/precache-manifest.3e7bbbc71a422fbb69eca0422b68825c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8c7ce28ad5aa2607e9ff567c1edcdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c04344597228f291eca6",
+    "url": "/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04344597228f291eca6",
+    "url": "/static/js/main.a1d195f9.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
